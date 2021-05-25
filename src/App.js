170419c5b696211bbbc884730c6b455384f8f1e7
mdestafadilah <desta.08b@gmail.com>
2021-05-26 04:36:12 +0700

    Mockup Kuis

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import './App.css';
-import { Card, CardHeader, CardBody } from 'reactstrap';
+import { Card, CardHeader, CardBody, Button, Container, Row, Col } from 'reactstrap';
 
 // fake data
 import { quiz } from './data/kuis'
 
-function App() {
+const App = () => {
+
+  const {id, question, options} = quiz[0];
+
   return (
     <div className="App">
-      <div class="kuis">
+      <Container fluid={true}>
         <h2 className="text-center mb-3 mt-3">Quiz Screen</h2>
         <Card>
-          <CardHeader>Ini adalah Pertanyaan</CardHeader>
-          <CardBody>Ini adalah body</CardBody>
+          <CardHeader>{question}</CardHeader>
+          <CardBody>
+            {options.map(item => (
+              <div style={{
+                display: 'flex',
+                justifyItems: 'center'
+              }}><div style={{
+                  height: 20,
+                  width: 20,
+                  borderRadius: 100,
+                  backgroundColor: "grey",
+                  cursor: "pointer",
+                  marginRight: 5
+              }}/>{item.title}</div>
+            ))}
+          </CardBody>
         </Card>
-      </div>
+        <div style={{
+          display: "flex",
+          justifyItems: "space-between"
+        }}>
+          <Row>
+            <Col><Button outline color="info" >Sebelumnya</Button></Col>
+            <Col><Button outline color="primary" >Selanjutnya</Button></Col>
+          </Row>
+        </div>
+      </Container>
     </div>
   );
 }

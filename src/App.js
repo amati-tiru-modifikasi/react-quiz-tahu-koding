96353076130564f24de182e324f7981393f7be73
mdestafadilah <desta.08b@gmail.com>
2021-05-26 04:08:22 +0700

    Tampilan Card Kuis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, CardHeader, CardBody } from 'reactstrap';
+
+// fake data
+import { quiz } from './data/kuis'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="kuis">
+        <h2 className="text-center mb-3 mt-3">Quiz Screen</h2>
+        <Card>
+          <CardHeader>Ini adalah Pertanyaan</CardHeader>
+          <CardBody>Ini adalah body</CardBody>
+        </Card>
+      </div>
     </div>
   );
 }

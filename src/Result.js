a54de4aa5408a363d1683aaaf0ccc42a87bafc0b
mdestafadilah <desta.08b@gmail.com>
2021-05-26 23:41:32 +0700

    Tampilkan Hasil Pilihan

--- src/Result.js
+++ src/Result.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import {  withRouter, useHistory } from 'react-router-dom';
+import { Card, CardHeader, CardBody, CardFooter } from 'reactstrap';
+
 
 const Result = ({location}) => {
     const history = useHistory();
@@ -8,9 +10,38 @@ const Result = ({location}) => {
         if(!location.state) history.push('/')
     },[])
 
-    return (
-        <div>
-            {JSON.stringify(location.state)}
+    return !location.state ? false : (
+        <div className="mt-3">
+            <h1 className="text-center mb-3">Hasil Kuesioner</h1>
+            {location.state.quiz.map((item,index) => (
+                <Card  className="mb-3" key={index}>
+                    <CardHeader>
+                        <div>No.{index + 1}</div> {item.question}
+                    </CardHeader>
+                    <CardBody>
+                    {item.options.map((item,index) => (
+                        <div style={{
+                            display: 'flex',
+                            justifyItems: 'center'
+                        }}
+                        key={index}
+                        ><div style={{
+                            height: 20,
+                            width: 20,
+                            borderRadius: 100,
+                            backgroundColor: item?.selected ? "greenyellow" : "grey",
+                            marginRight: 5,
+                            fontSize: 18
+                        }}
+                        />{item.title}</div>
+                    ))}
+                    </CardBody>
+                    <CardFooter>
+                        Benar salah
+                    </CardFooter>
+                </Card>
+            ))}
+            
         </div>
     )
 }
